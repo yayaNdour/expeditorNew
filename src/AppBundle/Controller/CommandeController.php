d1ab9c5fa55d72e2a466f435b9fa1cd2f16eeac6
sylvainRault2014 <sylvain.rault2014@campus-eni.fr>
2016-06-22 11:30:18 +0200

    Ajout des routes et controller

--- src/AppBundle/Controller/CommandeController.php
+++ src/AppBundle/Controller/CommandeController.php
@@ -2,18 +2,45 @@
 
 namespace AppBundle\Controller;
 
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 
 class CommandeController extends Controller {
-
     
-    public function indexAction(){
-         return $this->render('default/index.html.twig', array(
-            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
-        ));
+    public function showAction($id)
+    {
+        return new Response('<html><body>Commande !</body></html>');
+    }
+    
+    public function freeAction($id)
+    {
+        return new Response('<html><body>free Commande !</body></html>');
+    }
+    
+    public function updateAction($id)
+    {
+        return new Response('<html><body>update Commande !</body></html>');
+    }
+    
+    public function finishAction($id)
+    {
+        return new Response('<html><body>finish Commande !</body></html>');
+    }
+    
+    public function printAction($id)
+    {
+        return new Response('<html><body>print Commande !</body></html>');
+    }
+    
+    public function indexAction()
+    {
+        return new Response('<html><body>Liste commandes !</body></html>');
+    }
+    
+    public function startAction()
+    {
+        return new Response('<html><body>Commencer commandes !</body></html>');
     }
     
 }
